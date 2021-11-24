d91ce8f335100958ba65d39168f73c832c1e677d
Denick <1049485+apie@users.noreply.github.com>
2021-11-24 19:51:08 +0100

    add lastfm_loved.py

---
+++ lastfm_loved.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# Scrape the list with loved tracks for a user
+# By Apie
+# 2021-03-22
+
+import requests
+from lxml import html
+
+TIMEOUT = 12
+
+session = requests.Session()
+a = requests.adapters.HTTPAdapter(max_retries=3)
+session.mount('https://', a)
+
+
+def get_loved(username):
+    loved_tracks = []
+    next_button = True
+    page_nr = 1
+    while next_button:
+        url = f"https://www.last.fm/user/{username}/loved?page={page_nr}"
+        page = session.get(url, timeout=TIMEOUT).text
+        doc = html.fromstring(page)
+        l = zip(
+            doc.xpath("//tr/td[@class='chartlist-image']/*[@class='cover-art']/img"),
+            doc.xpath("//tr/td[@class='chartlist-name']/a"),
+            doc.xpath("//tr/td[@class='chartlist-artist']/a"),
+        )
+        loved_tracks.extend(list(map(lambda e: e.text.strip() if e.text else e.attrib.get('alt'), elements)) for elements in l)
+        next_button = doc.xpath("//li[@class='pagination-next']")
+        page_nr += 1
+        
+    return loved_tracks
+
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) < 2:
+        raise Exception('Give username as argument.')
+    stats = get_loved(argv[1])
+    for stat in stats:
+        print(';'.join(stat))
